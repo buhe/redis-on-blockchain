20459ae9cb829f7a1752c12b5422ac86c23b361d
buhe <bugu1986@126.com>
2022-07-15 16:50:40 +0800

    git-u:update

--- redis-contract/contracts/Redis.sol
+++ redis-contract/contracts/Redis.sol
@@ -17,4 +17,14 @@ contract Redis {
     function get(string memory key) public view returns (string memory) {
         return str_kv[msg.sender][key];
     }
+    string private greeting;
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        console.log("Changing greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
 }
--- redis-contract/scripts/deploy.ts
+++ redis-contract/scripts/deploy.ts
@@ -1,20 +1,29 @@
 import { ethers } from "hardhat";
 import fs from "fs";
 const fsPromises = fs.promises;
+function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  // const lockedAmount = ethers.utils.parseEther("1");
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // const Lock = await ethers.getContractFactory("Lock");
+  // const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 
-  await lock.deployed();
+  // await lock.deployed();
 
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  // console.log("Lock with 1 ETH deployed to:", lock.address);
+
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
 
   const Redis = await ethers.getContractFactory("Redis");
   const redis = await Redis.deploy();
@@ -22,7 +31,21 @@ async function main() {
   await redis.deployed();
 
   console.log("Redis deployed to:", redis.address);
-  await writeRust(redis.address, ['../redis-bridge/src/address.rs'])
+  await writeRust(redis.address, ['../redis-bridge/src/address.rs']);
+
+  await redis.set("key", "rob");
+  // wait block created
+  await delay(120 * 1000);
+
+  const val = await redis.get("key");
+
+  console.log("value is " + val);
+
+  await redis.setGreeting('hi bugu');
+
+  await delay(120 * 1000);
+
+  console.log("greet:", await redis.greet());
 }
 
 async function writeRust(address: string, files: string[]) {
